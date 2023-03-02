30ca90d792221a3282d74a95c3cbc12d4fc0ab7f
HuahChoi <115608641+HuahChoi@users.noreply.github.com>
2023-03-02 11:51:56 +0900

    Add files via upload

--- web.js
+++ web.js
@@ -6,40 +6,28 @@ class Server {
     // 생성자
     constructor() {
       this.app = express();
-    }
-  }
-  
-  const server = new Server().app;
-  
-  server.use(express.json()); 
-  server.use(express.urlencoded({ extended: false }));
-  server.use(cors()); //모든 cross-origin 요청에 대해 응답
-  server.use((req, res) => {
-    res.header("Access-Control-Allow-Origin", "*"); // 모든 도메인
-    });
-  
-  server.set('port', 80);
-  server.use(express.urlencoded({ extended: true }));
+      this.app.use(cors()); //모든 cross-origin 요청에 대해 응답
+      this.app.use(express.json());
 
-  // (post) QR 코드 찍었을 때
-  server.post('/qr', async (req,res)=>{
-    console.log('qr');
-    const userId = req.body.userId;
-    const code = req.body.code;
-    if (!code) res.json({message:"오류!"});
-    try {
-        // 특정 장소 DB 정보 업데이트 및 포인트 증가
-        await MainService.updateVisitedStatus(code);
-        // 방문 코스 체크
-        db.query(`UPDATE bdcb_members SET ${code}='Y' WHERE id='${userId}'`);
-        // 포인트 적립
-        db.query(`UPDATE bdcb_members SET point=point+500 WHERE id='${userId}'`);
-        return res.status(200).json({ code: "OK", message: "방문 완료" });
-    } catch (e) {
-        if (e.code) return res.status(e.status).json({ code: e.code, message: e.message });
-        else return res.json({message:"오류!"});
-    }
-})
+      // (post) QR 코드 찍었을 때
+      this.app.post('/qr', async (req,res)=>{
+            console.log('qr');
+            const userId = req.body.userId;
+            const code = req.body.code;
+            if (!code) res.json({message:"오류!"});
+            try {
+                // 특정 장소 DB 정보 업데이트 및 포인트 증가
+                await MainService.updateVisitedStatus(code);
+                // 방문 코스 체크
+                db.query(`UPDATE bdcb_members SET ${code}='Y' WHERE id='${userId}'`);
+                // 포인트 적립
+                db.query(`UPDATE bdcb_members SET point=point+500 WHERE id='${userId}'`);
+                return res.status(200).json({ code: "OK", message: "방문 완료" });
+            } catch (e) {
+                if (e.code) return res.status(e.status).json({ code: e.code, message: e.message });
+                else return res.json({message:"오류!"});
+            }
+        })
 
 // sns 로그인 체크(미완)
 /* app.post('/snslog',(req,res)=>{
@@ -61,121 +49,134 @@ class Server {
 }) */
 
 // 로그인 체크
-server.post('/logcheck',(req,res)=>{
-    console.log('logcheck')
-    const { logid, logpass } = req.body;
-    db.query(`select * from bdcb_members where id=${logid}`, (err,data) => {
-        //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
-        if (!data) res.json({ logchk: "false", error: "아이디 오류!" });
-        if (!err) {
-            if (data.pass === logpass) {
-                res.json({ logchk: "true", userInfo: data });
-            } else {
-                res.json({ logchk: "false", error: "비밀번호 오류!" });
-            }
-        } else {
-            res.json({ logchk: "false", error: "통신에러!" });
-        }
-    })
-})
-server.get('/logcheck',(req,res)=>{
-    console.log('logcheck');
-    res.send('아무');
-})
+        this.app.post('/logcheck',(req,res)=>{
+            console.log('logcheck')
+            const { logid, logpass } = req.body;
+            db.query(`select * from bdcb_members where id=${logid}`, (err,data) => {
+                //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
+                if (!data) res.json({ logchk: "false", error: "아이디 오류!" });
+                if (!err) {
+                    if (data.pass === logpass) {
+                        res.json({ logchk: "true", userInfo: data });
+                    } else {
+                        res.json({ logchk: "false", error: "비밀번호 오류!" });
+                    }
+                } else {
+                    res.json({ logchk: "false", error: "통신에러!" });
+                }
+            })
+        })
+        this.app.get('/logcheck',(req,res)=>{
+            console.log('logcheck');
+            res.send('아무');
+        })
 
-// 코스 정보 불러오기
-server.post('/coursedata',(req,res)=>{
-    console.log('/coursedata')
-    const { logid } = req.body;
-    db.query(`select * from bdcb_members where id=${logid}`, (err,data) => {
-        //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
-        if (!err) {
-            res.json({ chk: "true", userInfo: data });
-        } else {
-            res.json({ chk: "false", error: "통신에러!" });
-        }
-    })
-})
+        // 코스 정보 불러오기
+        this.app.post('/coursedata',(req,res)=>{
+            console.log('/coursedata')
+            const { logid } = req.body;
+            db.query(`select * from bdcb_members where id=${logid}`, (err,data) => {
+                //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
+                if (!err) {
+                    res.json({ chk: "true", userInfo: data });
+                } else {
+                    res.json({ chk: "false", error: "통신에러!" });
+                }
+            })
+        })
 
-// myroom 정보 불러오기
-server.post('/myroom',(req,res)=>{
-    console.log('myroom')
-    const { userId } = req.body;
-    db.query(`select * from bdcb_members where id=${userId}`, (err,data) => {
-        //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
-        if (!err) {
-            res.json({ chk: "true", userInfo: data });
-        } else {
-            res.json({ chk: "false", error: "통신에러!" });
-        }
-    })
-})
+        // myroom 정보 불러오기
+        this.app.post('/myroom',(req,res)=>{
+            console.log('myroom')
+            const { userId } = req.body;
+            db.query(`select * from bdcb_members where id=${userId}`, (err,data) => {
+                //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
+                if (!err) {
+                    res.json({ chk: "true", userInfo: data });
+                } else {
+                    res.json({ chk: "false", error: "통신에러!" });
+                }
+            })
+        })
 
-// 회원가입
-server.post('/signup',(req,res)=>{
-    const { username, email, callnum, id, pw} = req.body;
-    console.log('signup')
-    db.query(`select * from bdcb_members where id=${id}`, (err,data) => {
-        //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
-        if (!err) {
-            // DB에 일치 아이디가 없을 때
-            if (!data) {
-                db.query(`INSERT INTO 'bdcb_members' ('name', 'id', 'pass', 'email', 'tel', 'log_form', 'point') VALUES (${username}, ${id}, ${pw}, ${email}, ${callnum}, 'basic', 0)`, (err,data) => {
-                    //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
-                    if (!err) {
-                        db.query(`select * from bdcb_members where id=${id}`, (err,data) => {
+        // 회원가입
+        this.app.post('/signup',(req,res)=>{
+            const { username, email, callnum, id, pw} = req.body;
+            console.log('signup')
+            db.query(`select * from bdcb_members where id=${id}`, (err,data) => {
+                //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
+                if (!err) {
+                    // DB에 일치 아이디가 없을 때
+                    if (!data) {
+                        db.query(`INSERT INTO 'bdcb_members' ('name', 'id', 'pass', 'email', 'tel', 'log_form', 'point') VALUES (${username}, ${id}, ${pw}, ${email}, ${callnum}, 'basic', 0)`, (err,data) => {
                             //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
                             if (!err) {
-                                //console.log(data)//쿼리실행문 결과
-                                res.json({ logchk: "true", userInfo: data });
+                                db.query(`select * from bdcb_members where id=${id}`, (err,data) => {
+                                    //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
+                                    if (!err) {
+                                        //console.log(data)//쿼리실행문 결과
+                                        res.json({ logchk: "true", userInfo: data });
+                                    } else {
+                                        res.json({ logchk: "false", error: "통신에러!(최종호출)" });
+                                    }
+                                })
                             } else {
-                                res.json({ logchk: "false", error: "통신에러!(최종호출)" });
+                                res.json({ logchk: "false", error: "통신에러!(삽입)" });
                             }
                         })
                     } else {
-                        res.json({ logchk: "false", error: "통신에러!(삽입)" });
+                        res.json({ logchk: "false", error: "가입된 아이디입니다." });
+                    };
+                } else {
+                    res.json({ logchk: "false", error: "통신에러!(가입폼)" });
+                }
+            })
+        })
+
+        // 찜 목록 체크
+        this.app.post('/wishup',(req,res)=>{
+            const userId = localStorage.getItem('userId');
+            const { wshnum, wshchk } = req.body;
+            console.log('wishup')
+            if (wshchk === 'Y') {
+                db.query(`UPDATE bdcb_members SET ${wshnum}='Y' WHERE id='${userId}'`, (err,data) => {
+                    //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
+                    if (!err) {
+                        //console.log(data)//쿼리실행문 결과
+                        res.send(data)
+                    } else {
+                        console.log(err)
                     }
                 })
             } else {
-                res.json({ logchk: "false", error: "가입된 아이디입니다." });
-            };
-        } else {
-            res.json({ logchk: "false", error: "통신에러!(가입폼)" });
-        }
-    })
-})
-
-// 찜 목록 체크
-server.post('/wishup',(req,res)=>{
-    const userId = localStorage.getItem('userId');
-    const { wshnum, wshchk } = req.body;
-    console.log('wishup')
-    if (wshchk === 'Y') {
-        db.query(`UPDATE bdcb_members SET ${wshnum}='Y' WHERE id='${userId}'`, (err,data) => {
-            //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
-            if (!err) {
-                //console.log(data)//쿼리실행문 결과
-                res.send(data)
-            } else {
-                console.log(err)
+                db.query(`UPDATE bdcb_members SET ${wshnum}='N' WHERE id='${userId}'`, (err,data) => {
+                    //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
+                    if (!err) {
+                        //console.log(data)//쿼리실행문 결과
+                        res.send(data)
+                    } else {
+                        console.log(err)
+                    }
+                })
             }
         })
-    } else {
-        db.query(`UPDATE bdcb_members SET ${wshnum}='N' WHERE id='${userId}'`, (err,data) => {
-            //연결된 db에 query문 날리고 결과는 콜백에 들어온다.
-            if (!err) {
-                //console.log(data)//쿼리실행문 결과
-                res.send(data)
-            } else {
-                console.log(err)
-            }
+
+        this.app.listen(PORT,()=>{
+            console.log(`Server On: http://localhost:${PORT}`);
         })
     }
-})
+}
+  
+const server = new Server().app;
 
-server.listen(PORT,()=>{
-    console.log(`Server On: http://localhost:${PORT}`);
-})
+server.use((req, res) => {
+    res.header("Access-Control-Allow-Origin", "*"); // 모든 도메인
+});
+  
+server.set('port', 80);
+server.use(express.urlencoded({ extended: true }));
+
+  
 
 /* 
 app.use(cors());
